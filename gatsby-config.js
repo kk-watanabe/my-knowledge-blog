013053fa3be914b5fd278fb6b6429284c5e59bc8
kk-watanabe <kazuki.watanabe@bebit.com>
2021-09-24 17:20:22 +0900

    fix: Change new starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,5 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
+  // don't need to define it here (just if you need to change the options)
   plugins: [],
-}
+};
